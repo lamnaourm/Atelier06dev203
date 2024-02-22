58f8eb71e81d0cbb1c39fadaa82ce348f6c593e0
lamnaourm <mohammed.lamnaour@gmail.com>
2024-02-22 12:41:18 +0100

    Director schema

--- models/Director.js
+++ models/Director.js
@@ -2,7 +2,10 @@ import { Schema, model } from "mongoose";
 
 
 const DirectorSchema = new Schema({
-
+    name:{type:String, unique:true},
+    birthname:{type:String, required:true},
+    birthdate:Date,
+    birthplace:{type:String, default:'Casablanca', minLength:4},
 })
 
 export default model('director', DirectorSchema)
