b2604b4623a7af6e4959bb2aac9fa1f4175ec342
lamnaourm <mohammed.lamnaour@gmail.com>
2024-02-22 12:55:55 +0100

    routes

--- routes/Director.js
+++ routes/Director.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import DirectorModel from '../models/Director.js'
 
 const routes = express.Router()
 
 routes.get('/all', (req, res) => {
-   
+    DirectorModel.find({})
+    .then((directors) => {
+       return res.status(201).json(directors)
+    })
+    .catch((err) => {
+       return res.status(510).send('Erreur ...')
+    })
 })
 
 routes.get('/names', (req, res) => {
--- routes/Movie.js
+++ routes/Movie.js
@@ -1,16 +1,23 @@
 import express from 'express'
+import MovieModel from '../models/Movie.js'
 
 const routes = express.Router()
 
 routes.get('/all', (req, res) => {
-   
+    MovieModel.find({})
+    .then((movies) => {
+       return res.status(201).json(movies)
+    })
+    .catch((err) => {
+       return res.status(510).send('Erreur ...')
+    })
 })
 
 routes.get('/actors/:filmname', (req, res) => {
    
 })
 
-routes.years('/years/:year1/:year2', (req, res) => {
+routes.get('/years/:year1/:year2', (req, res) => {
    
 })
 
